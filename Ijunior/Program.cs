b5e114f7883cc8b04603c1d035621fe624436bd4
HemyDev <97851451+Elezthem@users.noreply.github.com>
2023-08-25 22:38:19 +0300

    Add files via upload

---
+++ Ijunior/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("4 плюс 6 = " + 10);
+Console.WriteLine("4 уменьшенное на 1 = " + - 4);
+Console.WriteLine("3 умножить на 7 = " + 21);
+Console.WriteLine("16 разделить на 8 = " + 2);
+Console.WriteLine("11 разделить на 5 = " + 2,2);
+Console.WriteLine("целая часть от деления 13 на 3 = " + 4);
+Console.WriteLine("остаток от деления 17 на 8 = " + 2);
